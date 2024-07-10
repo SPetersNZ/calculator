2b74023bb5a5ac7fc8d6bb553c24005147e8f4f6
SPetersNZ <137010917+SPetersNZ@users.noreply.github.com>
2024-07-11 11:15:07 +1200

    Adjustments to display output as below Top 'equationLine' will show whole equation Bottom 'answerLine' will show current input only

--- script.js
+++ script.js
@@ -1,19 +1,77 @@
 let firstString = "";
+let secondString = "";
+let answer = "";
+let equation = "";
+const mathOperations = "÷−×+";
+const numbers = ".0123456789";
 
-const button = document.querySelectorAll(".numPad");
+const numButton = document.querySelectorAll(".numPad");
+const operationButton = document.querySelectorAll(".operation");
+const equationLine = document.querySelector("#equationLine");
 const answerLine = document.querySelector("#answerLine");
 
-button.forEach((button) => {
-    button.addEventListener("click", updateString);
+numButton.forEach((button) => {
+    button.addEventListener("click", getInput);
 });
 
-function updateString (e) {
+operationButton.forEach((button) => {
+    button.addEventListener("click", getInput);
+});
+
+function getInput (e) {
     var element = e.target;
     var text = element.innerText;
-    firstString = firstString + text;
-    updateDisplay(firstString);
+    if (mathOperations.includes(text)) {
+        e.target.value = "true";
+    }
+    updateAnswerLine(text, e);
+    updateEquationLine(text);
 };
 
-function updateDisplay(text) {
-    answerLine.innerText = firstString;
+function updateAnswerLine(text, e) {
+    var element = e.target;
+    var test = element.value;
+    if (test === "&equals;") { 
+        operate(firstString, secondString); 
+    } else if (anyMathOperationsIncluded(equationLine.innerText, mathOperations)) { 
+        secondString = secondString + text;
+        let cleanedSecondString = secondString
+            .split("")
+            .filter((char) => numbers.includes(char))
+            .join("");
+        answerLine.innerText = cleanedSecondString;
+    } else {
+        firstString = firstString + text;
+        let cleanedFirstString = firstString
+            .split("")
+            .filter((char) => numbers.includes(char))
+            .join("");
+        answerLine.innerText = cleanedFirstString;
+    }
+    console.log("First: " + firstString);
+    console.log("Second: " + secondString);
+    console.log("Text: " + text);
+    // console.log("true/false: " + test);
 };
+
+function updateEquationLine(text) {
+    equation = equation + text;
+    equationLine.innerText = equation; 
+}
+
+function operate(firstString, secondString) {
+    alert("This is a test!");
+};
+
+function plus(firstString, secondString) {
+    return firstString + secondString;
+};
+
+function anyMathOperationsIncluded(str, chars) {
+    for (let char of chars) {
+        if (str.includes(char)) {
+            return true;
+        }
+    }
+    return false;
+}
