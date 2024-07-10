665b4062cb5480e216bea53d89428ab99d6e864c
SPetersNZ <137010917+SPetersNZ@users.noreply.github.com>
2024-07-10 12:08:34 +1200

    Display now strings together multiple inputs

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+let firstString = "";
+
 const button = document.querySelectorAll(".numPad");
 const answerLine = document.querySelector("#answerLine");
 
 button.forEach((button) => {
-    button.addEventListener("click", testFunction);
+    button.addEventListener("click", updateString);
 });
 
-function testFunction (e) {
+function updateString (e) {
     var element = e.target;
     var text = element.innerText;
-    answerLine.innerText = text;
+    firstString = firstString + text;
+    updateDisplay(firstString);
+};
+
+function updateDisplay(text) {
+    answerLine.innerText = firstString;
 };
